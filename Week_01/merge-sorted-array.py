b832b933f8e102de82ad3515e0a9752408227890
yudongdai <41011603@qq.com>
2020-06-28 20:23:15 +0800

    合并两个有序数组

---
+++ Week_01/merge-sorted-array.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+
+        nums1_copy = nums1[:m]
+
+        p1 = 0
+        p2 = 0
+        while p1 < m and p2 < n:
+            if nums1_copy[p1] < nums2[p2]:
+                nums1[p1 + p2] = nums1_copy[p1]
+                p1 += 1
+            else:
+                nums1[p1 + p2] = nums2[p2]
+                p2 += 1
+        
+        while p1 < m:
+            nums1[p1 + p2] = nums1_copy[p1]
+            p1 += 1
+        
+        while p2 < n:
+            nums1[p1 + p2] = nums2[p2]
+            p2 += 1
+# 3
