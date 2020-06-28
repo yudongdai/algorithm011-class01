431ce3665b50b55090f481d4683513b72691117f
yudongdai <41011603@qq.com>
2020-06-28 20:11:07 +0800

    合并两个有序链表

---
+++ Week_01/merge-two-sorted-lists.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        
+        l3 = ListNode(-1)
+        head = l3
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                l3.next = l1
+                l1 = l1.next
+            else:
+                l3.next = l2
+                l2 = l2.next
+            l3 = l3.next
+
+        l3.next = l1 or l2
+        return head.next
+
+#3
