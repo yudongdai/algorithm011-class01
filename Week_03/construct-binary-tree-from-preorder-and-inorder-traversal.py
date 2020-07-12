1035bd9c95fa15d95486f794de45627f6128703f
yudongdai <41011603@qq.com>
2020-07-12 20:13:08 +0800

    从前序与中序遍历序列构造二叉树

---
+++ Week_03/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+
+        if not preorder or not inorder:
+            return None
+
+        root_val = preorder[0]
+        root = TreeNode(root_val)
+
+        index = inorder.index(root_val)
+
+        root.left = self.buildTree(preorder[1:index + 1], inorder[:index])
+        root.right = self.buildTree(preorder[index + 1:], inorder[index + 1:])
+
+        return root
+        
+# 2
